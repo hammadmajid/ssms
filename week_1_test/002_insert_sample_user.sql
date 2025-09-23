1024db5d78a78b14ea8133b45b3cb74c3dda6a1f
Hammad Majid <hammadmajid@proton.me>
2025-09-23 20:22:44 +0500

    week 1 test

---
+++ week_1_test/002_insert_sample_user.sql
@@ -0,0 +1,7 @@
+use testdb_v0;
+
+INSERT INTO users (Id, Name, Email)
+VALUES
+(1, 'alice', 'alice@example.com'),
+(2, 'bob', 'bob@example.com'),
+(3, 'charlie', 'charlie@example.com');
---
+++ week_1_test/003_select_users.sql
@@ -0,0 +1,7 @@
+USE [testdb_v0]
+
+SELECT [Id]
+      ,[Email]
+      ,[Name]
+      ,[Dob]
+  FROM [dbo].[users]
