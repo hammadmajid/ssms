27d62b39df521e39570cac09c6e757a560303cac
Hammad Majid <hammadmajid@proton.me>
2025-10-17 15:28:33 +0500

    lab 5 db

---
+++ week_4/001_create_table_course.sql
@@ -0,0 +1,3 @@
+USE Lab5;
+
+CREATE TABLE Course(courseid int primary key, course_name varchar(50), credit_hourss varchar(5))
---
+++ week_4/002_insert_data_in_course.sql
@@ -0,0 +1,3 @@
+USE Lab5;
+
+INSERT INTO Course(courseid, course_name, credit_hourss) values (1, 'DSA', '3,0');
