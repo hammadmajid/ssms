4598a5e050df901e2248466297f9b830bcb6a906
Hammad Majid <hammadmajid@proton.me>
2026-01-01 21:45:38 +0500

    delete invalid code

--- paper-2/q2/006_part_b.sql
+++
@@ -1,9 +0,0 @@
-USE UniDB;
-
-SELECT Department.DeptName, Buildings.BuildingName
-FROM Professors 
-LEFT JOIN Department
-ON Department.DeptID = Professors.DeptID
-INNER JOIN Buildings
-ON Department.BuildingID = Buildings.BuildingID
-WHERE Professors.DeptID IS NULL;
